e216412933e660c2ba7d7f19fa3ca428b3f22caa
Mikhail Drobynin <mdrobynin@griddynamics.net>
2019-03-19 11:38:03 +0400

    added solver service

--- src/app/containers/game/game.component.ts
+++ src/app/containers/game/game.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {GeneratorService} from '../../services/generator.service';
+import {SolverService} from '../../services/solver.service';
 
 @Component({
 	selector: 'app-game',
@@ -10,7 +11,8 @@ import {GeneratorService} from '../../services/generator.service';
 export class GameComponent implements OnInit {
 	public sudoku: number[][];
 
-	constructor(private generatorService: GeneratorService) {
+	constructor(private generatorService: GeneratorService,
+				private solverService: SolverService) {
 	}
 
 	ngOnInit() {
@@ -19,5 +21,20 @@ export class GameComponent implements OnInit {
 
 	public generateSudoku(): void {
 		this.sudoku = this.generatorService.generateSudoku();
+		const sudoku = '819..5.....2...75..371.4.6.4..59.1..7..3.8..2..3.62..7.5.7.921..64...9.....2..438'
+			.split('.').join('0');
+		const result = [];
+
+		for (let i = 0; i < 9; i++) {
+			const subarr = [];
+
+			for (let j = 0; j < 9; j++) {
+				subarr.push(sudoku[i * 9 + j]);
+			}
+
+			result.push(subarr);
+		}
+
+		this.solverService.solveSudoku(result).then(console.log);
 	}
 }
